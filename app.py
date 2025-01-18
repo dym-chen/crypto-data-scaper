5760be59268fc6cee21f1fc5654397445d354d10
dym-chen <danielymchen@gmail.com>
2025-01-18 18:29:28 -0500

    functionality to scrape data off the website

--- app.py
+++ app.py
@@ -17,7 +17,23 @@ def main():
             page.mouse.wheel(0, 2000)
             page.wait_for_timeout(1000)
 
-        page.wait_for_timeout(5000)
+        trs_xpath = "//table[@class = 'sc-936354b2-3 tLXcG cmc-table']/tbody/tr"
+        trs_list = page.query_selector_all(trs_xpath)
+
+        data_list = []
+
+        for coin in trs_list:
+           coin_dict = {}   
+           
+           tds = coin.query_selector_all('//td')
+
+           coin_dict['id'] = tds[1].inner_text()
+           coin_dict['name'] = tds[2].query_selector("//p[contains(@class, 'coin-item-name')]").inner_text()
+           coin_dict['symbol'] = tds[2].query_selector("//p[contains(@class, 'coin-item-symbol')]").inner_text()
+
+           data_list.append(coin_dict)
+
+        print(data_list)
 
         # save data into postgres
 
